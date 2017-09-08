4c3eafa7d53e1894852a06497598c8dd4df44605
Sahil Sharma <sahil.sharma@careerone.com.au>
2017-09-08 16:56:34 +1000

    Adding some logical tests

--- tests/RepositoryTest.php
+++ tests/RepositoryTest.php
@@ -5,15 +5,34 @@
  */
 class RepositoryTest extends Tests\TestCase
 {
+    use \Pyntax\Traits\ServiceForResource;
+
     /**
      *
      */
     public function testCreatingARepositoryWithAModel()
     {
-        $config = config('pyntax-api-helper.policies.authProtectedForeignKey');
-        $this->assertEquals(
-            $config,
-            "users_id"
+        $this->assertInstanceOf(
+            \Pyntax\Contracts\Repositories\Repository::class,
+            $this->loadRepositoryForGivenResource('users')
         );
+    }
+
+    public function testCreatingAServiceWithAModel()
+    {
+        $this->assertInstanceOf(
+            \Pyntax\Contracts\Services\Service::class,
+            $this->loadServiceForGivenResource('users')
+        );
+    }
+
+    public function testCreateAUserUsingTheService()
+    {
+        $userEmail = 'SahildDude89ss@gmail.com';
+
+        $repository = $this->loadRepositoryForGivenResource('users');
+        $user = $repository->create(['first_name' => 'Dummy', 'last_name' => 'Dummy', 'email' => $userEmail]);
+
+        $this->assertAttributeEquals($userEmail, 'email', $user);
     }
 }
